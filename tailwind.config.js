958a44d5cf62b03f6480c88bc6ff7bf3342d3621
nitish1899 <nkword1899@gmail.com>
2025-05-25 10:33:45 +0530

    fixed animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       inter: ["Inter", "sans-serif"],
@@ -13,38 +11,19 @@ module.exports = {
     extend: {
       keyframes: {
         bgColorChange: {
-          '0%, 100%': { backgroundColor: '#00bf63' },  // light blue
-          '25%': { backgroundColor: '#ffde59' },        // light pink
-          '50%': { backgroundColor: '#004aad' },        // light beige
-          '75%': { backgroundColor: '#ff3131' },        // light green
+          "0%, 100%": { backgroundColor: "#00bf63" }, // light blue
+          "25%": { backgroundColor: "#ffde59" }, // light pink
+          "50%": { backgroundColor: "#004aad" }, // light beige
+          "75%": { backgroundColor: "#ff3131" }, // light green
         },
       },
       animation: {
-        'bg-color-change': 'bgColorChange 10s infinite',
+        "bg-color-change": "bgColorChange 10s infinite",
       },
       height: {
-        md: '400px', // now you can use h-md
+        md: "400px", // now you can use h-md
       },
     },
   },
   plugins: [],
-}
-
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: { 
-//     fontFamily: {
-//       inter: ["Inter", "sans-serif"],
-//       "edu-sa": ["Edu SA Beginner", "cursive"],
-//       mono: ["Roboto Mono", "monospace"],
-//     },
-//     extend: {
-//   },
-//   },
-//   plugins: [],
-// }
-
+};
